fbfd2ad8a40b509f6dd6e2b4219375bd24fcea62
Cat-H0lic <rda1024@yandex.ru>
2021-12-13 19:25:12 +0300

    Add project files.

---
+++ C_Sharp_Basics/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace C_Sharp_Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName;
+            MyName = "Dmitrii";
+            Console.WriteLine(MyName);
+        }
+    }
+}
